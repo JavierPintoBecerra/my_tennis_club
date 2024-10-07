119330c4a30a5c26cdfbeb21ff9d29bcef8204be
JavierPinto <javier.pinto.becerra@gmail.com>
2024-10-07 13:41:34 -0500

    Django make list display more readable

--- members/admin.py
+++ members/admin.py
@@ -2,4 +2,8 @@ from django.contrib import admin
 from .models import Member
 
 # Register your models here.
-admin.site.register(Member)
+
+class MemberAdmin(admin.ModelAdmin):
+    list_display = ("first_name", "last_name", "joined_date")
+
+admin.site.register(Member, MemberAdmin) 
