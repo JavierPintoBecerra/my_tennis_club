b293c0ab718a026d9cc8e4199d42e700cb3b63e6
JavierPinto <javier.pinto.becerra@gmail.com>
2024-10-08 13:59:31 -0500

    Testing template

--- members/views.py
+++ members/views.py
@@ -30,9 +30,11 @@ def main(request):
   return HttpResponse(template.render())
 
 
+#This section is for testing purposes only
 def testing(request):
+  mymembers = Member.objects.all().values()
   template = loader.get_template('template.html')
   context = {
-    'fruits': ['Apple', 'Banana', 'Cherry'],   
+    'mymembers': mymembers,
   }
   return HttpResponse(template.render(context, request))
